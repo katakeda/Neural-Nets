e2ae2d30761fcd570978cfb8ecf730a9c292b5eb
Kaitaku Takeda <katakeda@ucsc.edu>
2017-06-11 20:36:53 -0700

    Update to finalized version

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ var myNetwork = new Network({
 // Wrapper for activating the network and propagating
 var trainer = new Trainer(myNetwork);
 
-// Training set will be changed dynamically upon users request
 // It will be inputs and outputs for XOR on default
 var trainingSet = [
   {input: [0,0], output: [0]},
@@ -35,6 +34,35 @@ var trainingSet = [
   {input: [1,1], output: [0]}
 ]
 
+// Get weights from neuron in input layer to neurons in hidden layer
+function getInputWeights() {
+  var neuron1ProjectedTo = inputLayer.list[0].connections.projected;
+  var neuron2ProjectedTo = inputLayer.list[1].connections.projected;
+  var weights = {
+    w1: neuron1ProjectedTo[6].weight.toFixed(4),
+    w2: neuron1ProjectedTo[7].weight.toFixed(4),
+    w3: neuron1ProjectedTo[8].weight.toFixed(4),
+    w4: neuron2ProjectedTo[9].weight.toFixed(4),
+    w5: neuron2ProjectedTo[10].weight.toFixed(4),
+    w6: neuron2ProjectedTo[11].weight.toFixed(4)
+  }
+
+  return weights;
+}
+
+// Get weights from neurons in hidden layer to neurons in output layer
+function getHiddenWeights() {
+  var neuron1ProjectedTo = hiddenLayer.list[0].connections.projected;
+  var neuron2ProjectedTo = hiddenLayer.list[1].connections.projected;
+  var neuron3ProjectedTo = hiddenLayer.list[2].connections.projected;
+  var weights = {
+    w1: neuron1ProjectedTo[12].weight.toFixed(4),
+    w2: neuron2ProjectedTo[13].weight.toFixed(4),
+    w3: neuron3ProjectedTo[14].weight.toFixed(4)
+  }
+
+  return weights;
+}
 
 // General function for slider setup
 function setSlider(c) {
@@ -67,6 +95,10 @@ function setSlider(c) {
 }
 
 $(document).ready(function() {
+  var input1, input2;
+  var targetVal = $('#target-val');
+  var outputVal = $('#output-val');
+
   // Setup slider for rate
   setSlider('r');
 
@@ -75,6 +107,28 @@ $(document).ready(function() {
 
   // Setup slider for error
   setSlider('e');
+
+  // Detect change in the input values
+  window.setInterval(function(){
+    var inputVal = $('#input-select').val();
+    if(inputVal == "00") {
+      input1 = 0;
+      input2 = 0;
+      targetVal.text(0);
+    } else if(inputVal == "01") {
+      input1 = 0;
+      input2 = 1;
+      targetVal.text(1);
+    } else if(inputVal == "10") {
+      input1 = 1;
+      input2 = 0;
+      targetVal.text(1);
+    } else {
+      input1 = 1;
+      input2 = 1;
+      targetVal.text(0);
+    }
+  }, 300)
 
   // Train with values from sliders
   $('#btn-train').click(function() {
@@ -93,6 +147,6 @@ $(document).ready(function() {
 
   // Run with given input
   $('#btn-run').click(function() {
-    $('#output-val').text(myNetwork.activate([1, 1]));
+    outputVal.text(myNetwork.activate([input1, input2]));
   })
 })
--- js/sketch.js
+++ js/sketch.js
@@ -5,13 +5,27 @@ function setup() {
 function draw() {
   var x = Math.round($('#output-val').text());
 
+  // Weights
+  push();
+  textSize(12);
+  textAlign(CENTER, CENTER);
+  textFont("monospace");
+  fill(255, 255, 0);
+  text(getInputWeights().w1, 175, 175);
+  text(getInputWeights().w2, 190, 210);
+  text(getInputWeights().w3, 175, 250);
+  text(getInputWeights().w4, 175, 355);
+  text(getInputWeights().w5, 190, 395);
+  text(getInputWeights().w6, 175, 430);
+  text(getHiddenWeights().w1, 485, 150);
+  text(getHiddenWeights().w2, 485, 285);
+  text(getHiddenWeights().w3, 485, 450);
+  pop();
+
   // Input
   for(let i = 0; i < 2; i++) {
     push();
     ellipse(125, 200+(200*i), 65, 65);
-    textSize(30);
-    textAlign(CENTER, CENTER);
-    text(1, 125, 200+(200*i));
     pop();
   }
 
@@ -23,6 +37,10 @@ function draw() {
     line(125, 200, 425, 150+(150*i));
     line(125, 400, 425, 150+(150*i));
     line(425, 150+(150*i), 725, 300);
+    textSize(30);
+    textAlign(CENTER, CENTER);
+    textFont('Monospace');
+    text('S', 425, 150+(150*i));
     pop();
   }
 
@@ -32,5 +50,25 @@ function draw() {
   textSize(30);
   textAlign(CENTER, CENTER);
   text(x, 725, 300);
+  pop();
+
+  // Change input values
+  push();
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  var inputVal = $('#input-select').val();
+  if(inputVal == "00") {
+    text(0, 125, 200);
+    text(0, 125, 400);
+  } else if(inputVal == "01") {
+    text(0, 125, 200);
+    text(1, 125, 400);
+  } else if(inputVal == "10") {
+    text(1, 125, 200);
+    text(0, 125, 400);
+  } else {
+    text(1, 125, 200);
+    text(1, 125, 400);
+  }
   pop();
 }
